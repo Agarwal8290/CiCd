a9d4fc7df3f56ac17e6919332a3f25a28d278a02
Nitesh Agarwal <niteshagarwal8290@gmail.com>
2024-09-02 16:32:28 +0530

    fix issue

--- tests/Signin.spec.js
+++ tests/Signin.spec.js
@@ -22,7 +22,7 @@ test('invalid SignIn', async({page})=>{
     await page.getByRole('button', { name: 'Sign In' }).click();
     page.on('dialog', async dialog => {
         // Verify the alert message
-        expect(dialog.message()).toBe('Invalid username or password.');
+        expect(dialog.message()).toBe(' username or password.');
     
         // Accept the alert
         await dialog.accept();
@@ -36,7 +36,7 @@ test('journal page', async({page})=>{
   await page.getByPlaceholder('Password').fill('Nitesh@8209');
   await page.getByPlaceholder('Password').press('Enter');
   await page.getByRole('navigation').getByRole('link', { name: 'Journal' }).click();
-  await expect(page.locator('#content')).toContainText('Journal for this Garden new');
+  await expect(page.locator('#content')).toContainText('Journal for this Garden');
   await page.getByRole('link', { name: 'NA', exact: true }).click();
   await page.getByRole('link', { name: 'Log Out' }).click();
 })
