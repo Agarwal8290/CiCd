b0e96974a1954e20d0b2ec2f756596fb07d3e101
Nitesh Agarwal <niteshagarwal8290@gmail.com>
2024-09-02 17:10:54 +0530

    fix issue

--- tests/Signin.spec.js
+++ tests/Signin.spec.js
@@ -20,13 +20,10 @@ test('invalid SignIn', async({page})=>{
     await page.getByPlaceholder('Password').click();
     await page.getByPlaceholder('Password').fill('Nitesh@8290');
     await page.getByRole('button', { name: 'Sign In' }).click();
-    page.on('dialog', async dialog => {
-        // Verify the alert message
-        expect(dialog.message()).toBe(' username or password.');
-    
-        // Accept the alert
-        await dialog.accept();
-      });
+    const errorMessageLocator = page.locator('#flash .error');
+    await errorMessageLocator.waitFor({ state: 'visible' });
+    const errorMessage = await errorMessageLocator.textContent();
+    expect(errorMessage).toBe('Invalid username or pas');
 })
 
 test('journal page', async({page})=>{
