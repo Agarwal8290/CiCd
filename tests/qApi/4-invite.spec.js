c05f8f9b042c4ec966bd8feb3d2646100681888a
Nitesh Agarwal <niteshagarwal8290@gmail.com>
2024-12-10 15:26:10 +0530

    added cicle ci file

--- tests/qApi/4-invite.spec.js
+++ tests/qApi/4-invite.spec.js
@@ -1,25 +1,25 @@
-import { test, expect } from '@playwright/test';
+// import { test, expect } from '@playwright/test';
 
-test.describe.serial('New Todo', () => {
-  test('has title 7', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+// test.describe.serial('New Todo', () => {
+//   test('has title 7', async ({ page }) => {
+//     await page.goto('https://playwright.dev/');
   
-    // Expect a title "to contain" a substring.
-    await expect(page).toHaveTitle(/Playwright/);
-    console.log('has title 7');
+//     // Expect a title "to contain" a substring.
+//     await expect(page).toHaveTitle(/Playwright/);
+//     console.log('has title 7');
 
-  });
+//   });
   
-  test('get started link 8', async ({ page }) => {
-    await page.goto('https://playwright.dev/');
+//   test('get started link 8', async ({ page }) => {
+//     await page.goto('https://playwright.dev/');
   
-    // Click the get started link.
-    await page.getByRole('link', { name: 'Get started' }).click();
+//     // Click the get started link.
+//     await page.getByRole('link', { name: 'Get started' }).click();
   
-    // Expects page to have a heading with the name of Installation.
-    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-    console.log('has title 8');
+//     // Expects page to have a heading with the name of Installation.
+//     await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+//     console.log('has title 8');
 
-  });
+//   });
   
-  });
+//   });
